3bd612faf152b22e682c7a05ecefb6c28f0cde5c
Roland Szilágyi <75616702+roland-szilagyi@users.noreply.github.com>
2023-12-19 12:10:29 +0100

    feat_hamburger-toggle

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,28 @@
-// Hamburger toggle
 let hamburgerBtnOpen = document.querySelector('.js-ham-btn-open');
 hamburgerBtnOpen.addEventListener('click', hamburgerOpener);
 
 let hamburgerBtnClose = document.querySelector('.js-ham-btn-close');
-hamburgerBtnClose.addEventListener('click', hamburgerCloser); 
+hamburgerBtnClose.addEventListener('click', hamburgerCloser);
 
-// Megnyitja a hamburgert
+
 function hamburgerOpener() {
-  let hamburger = document.querySelector('.js-ham');
-    hamburger.classList.remove('hide');
-    hamburger.classList.add('show');
-    PagesCloser();
-};
-
-// Eltünteti a hamburgert
-function hamburgerCloser() {
-  let hamburger = document.querySelector('.js-ham');
-    hamburger.classList.remove('show');
-    hamburger.classList.add('hide');
-    PagesOpener();
-};
-
-// Eltünteti az összes oldalt ha a hamburgert megnyitjuk
-function PagesCloser() {
-  let pages = document.querySelectorAll('.js-page');
-    pages.forEach(function(page) {
+  let hamburgerIsOpen = document.querySelector('.js-ham');
+  let pageIsOpen = document.querySelectorAll('.js-page');
+  hamburgerIsOpen.classList.add('show');
+  hamburgerIsOpen.classList.remove('hide');
+  pageIsOpen.forEach(function(page) {
     page.classList.remove('show');
     page.classList.add('hide');
-  });
+  })
 };
 
-// Visszahozza az oldalt ami megvolt nyitva ha a hamburgert bezárjuk
-function PagesOpener() {
-  let pages = document.querySelectorAll('.active');
-    pages.forEach(function(page) {
+function hamburgerCloser() {
+  let hamburgerIsOpen = document.querySelector('.js-ham');
+  let pageIsOpen = document.querySelectorAll('.active');
+  hamburgerIsOpen.classList.add('hide');
+  hamburgerIsOpen.classList.remove('show');
+  pageIsOpen.forEach(function(page) {
     page.classList.remove('hide');
     page.classList.add('show');
-  });
+  })
 };
