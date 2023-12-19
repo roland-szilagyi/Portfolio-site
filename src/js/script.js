f7be5585d4c529ffda80039930e446b37784afdc
Roland Szilágyi <75616702+roland-szilagyi@users.noreply.github.com>
2023-12-19 08:13:23 +0100

    fix_hamburger-function-fixed

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,36 @@ hamburgerBtnOpen.addEventListener('click', hamburgerOpener);
 let hamburgerBtnClose = document.querySelector('.js-ham-btn-close');
 hamburgerBtnClose.addEventListener('click', hamburgerCloser); 
 
+// Megnyitja a hamburgert
 function hamburgerOpener() {
-  let links = document.querySelector('.js-ham-links');
-    links.classList.remove('hide');
-    links.classList.add('show');
+  let hamburger = document.querySelector('.js-ham');
+    hamburger.classList.remove('hide');
+    hamburger.classList.add('show');
+    PagesCloser();
 };
-    
+
+// Eltünteti a hamburgert
 function hamburgerCloser() {
-  let links = document.querySelector('.js-ham-links');
-    links.classList.remove('show');
-    links.classList.add('hide');
+  let hamburger = document.querySelector('.js-ham');
+    hamburger.classList.remove('show');
+    hamburger.classList.add('hide');
+    PagesOpener();
+};
+
+// Eltünteti az összes oldalt ha a hamburgert megnyitjuk
+function PagesCloser() {
+  let pages = document.querySelectorAll('.js-page');
+    pages.forEach(function(page) {
+    page.classList.remove('show');
+    page.classList.add('hide');
+  });
+};
+
+// Visszahozza az összes oldalt ha a hamburgert bezárjuk
+function PagesOpener() {
+  let pages = document.querySelectorAll('.js-page');
+    pages.forEach(function(page) {
+    page.classList.remove('hide');
+    page.classList.add('show');
+  });
 };
